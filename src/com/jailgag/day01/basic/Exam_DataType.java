ad00593b528dde0536046abf55485cd1b3b2d8cb
PC <PC@DESKTOP-49R6H6K>
2024-12-09 00:49:05 +0900

    [ADD] 연습문제 다시 풀기
    1. 테스트 입니다.

--- src/com/jailgag/day01/basic/Exam_DataType.java
+++ src/com/jailgag/day01/basic/Exam_DataType.java
@@ -1,35 +1,35 @@
 package com.jailgag.day01.basic;
 
 public class Exam_DataType {
+	//한줄주석 
+			/*
+			 * 여러줄 주석
+			 * 자료형의 분류 2가지
+			 * 1.기본형
+			 * -정수, 실수 문자 참/거짓
+			 * 정수 4가지 (byte, shot. int , long)
+			 * 실수 3가지 (float. double)
+			 * 문자 char
+			 * 참/거짓 boolean
+			 * 2.참조형, 주소값을 저장함.
+			 * -문자열 String
+			 */
 	public static void main(String[] args) {
-		//한줄주석 
-		/*
-		 * 여러줄 주석
-		 * 자료형의 분류 2가지
-		 * 1.기본형
-		 * -정수, 실수 문자 참/거짓
-		 * 정수 4가지 (byte, shot. int , long)
-		 * 실수 3가지 (float. double)
-		 * 문자 char
-		 * 참/거짓 boolean
-		 * 2.참조형, 주소값을 저장함.
-		 * -문자열 String
-		 */
 		boolean result = true;
 		char fChar = 'a';
 		System.out.println(result);
 		System.out.println(fChar);
-		//정수 part 
+		//정수 part
 		byte bNum = 127;
 		short sNum = 32767;
-		long lNum = 2147483648l;
+		long lNum = 214783648;
 		System.out.println(bNum);
 		System.out.println(sNum);
 		System.out.println(lNum);
 		//실수 part
 		float fNum = 25.52f;
 		double dNum = 25.52;
-		
+		System.out.println(fNum);
+		System.out.println(dNum);
 	}
-
 }
--- src/com/jailgag/day02/control/condition/exercise/Exercise_If1.java
+++ src/com/jailgag/day02/control/condition/exercise/Exercise_If1.java
@@ -6,27 +6,28 @@ public class Exercise_If1 {
 	/*
 	 * 문제1
 	 * 점수와 학년을 정수로 입력받아 60점 이상이면 합격
-	 * 60점 미만이면 불합격으로 출력한다.
-	 * 단, 4학년의 경우 70점 이상이어야 합격을 출력하는 프로그램을 작성하여라
-	 * 점수를 입력하세요(0~100) :89
+	 * 60점 미만이면 불합격으로 출력한다
+	 * 단, 4학년의 경우 70점 이상이어야 합격을 출력하는 프로그램을작성하여라
+	 * 
+	 * 점수를 입력하세요(0~100) : 89
 	 * 학년을 입력하세요(1~4) : 4
 	 * 합격입니다.
 	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("점수를 입력하세요(0~100) : ");
-		int score = sc.nextInt();
-		System.out.print("학년을 입력하세요(1~4) : ");
-		int grade = sc.nextInt(); 
-		// 4학년 
-		if(score >= 60) {
-			if(grade == 4 && score >=70) {
-				System.out.println("불합격입니다");   //출력시 70/4 입력 불합격 합격같이 출력
-				return;                           //return; 입력후 출력시 불합격만 출력
+		System.out.print("점수를 입력하세요 (0~100) : ");
+		int score = sc.nextInt();  //점수는 score
+		System.out.print("학년을 입력하세요 (1~4) : ");
+		int grade = sc.nextInt();  // 학년은 grade
+		if(score >= 60) {  // >= 등호기호??60이상 
+			if(grade ==4 && score >= 70) {  
+				System.out.println("불합격입니다");
+				return;
 			}
-			System.out.println("합격입니다.");
+			System.out.println("합격입니다");
 		}else {
-			System.out.println("불합격입니다.");
+			System.out.println("불합격입니다");
 		}
+		
 	}
 }
